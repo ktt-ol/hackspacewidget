1262d90df24f73f46a34bc79029f8559bf3e2b05
lhw <lhw+github@ring0.de>
2012-03-09 03:29:09 +0100

    now using gson

--- src/de/ring0/hackspace/HackerSpaceStatusAPI.java
+++ src/de/ring0/hackspace/HackerSpaceStatusAPI.java
@@ -4,116 +4,77 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-public class HackerSpaceStatusAPI implements ResponseHandler<String> {
-	private final static String TAG = HackerSpaceStatusAPI.class.getSimpleName();
+import com.google.gson.Gson;
+
+public class HackerSpaceStatusAPI implements ResponseHandler<Boolean>, Runnable {
+//	private final static String TAG = HackerSpaceStatusAPI.class.getSimpleName();
 	private final static String VERSION = "0.1";
 	
 	private APICallback apiCallback;
+	private HttpClient client;
+	private HttpGet get;
 	
-	public HackerSpaceStatusAPI(String url, APICallback callback) throws ClientProtocolException, IOException {
+	public HackerSpaceStatusAPI(String url, APICallback callback) {
 		apiCallback = callback;
-		HttpClient client = new DefaultHttpClient();
-		HttpGet get = new HttpGet(url);
+		client = new DefaultHttpClient();
+		get = new HttpGet(url);
 		get.setHeader("Accept", "application/json");
 		get.setHeader("User-Agent", String.format("Android Widget/%s", VERSION));
-		client.execute(get, this);
 	}
-
+	
 	@Override
-	public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+	public void run() {
+		try {
+			client.execute(get, this);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	@Override
+	public Boolean handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
 		HttpEntity body;
-		JSONObject job;
-		String line = null, content = null;
-		
-		if((body = response.getEntity()) != null) {
+		String line, content = "";
+
+		if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK && (body = response.getEntity()) != null) {
 			BufferedReader bir = new BufferedReader(new InputStreamReader(body.getContent()));
 			while((line = bir.readLine()) != null) {
 				content += line;
 			}
 			bir.close();
-			
-			try {
-				job = new JSONObject(content);
-				HSSAPIHandler handler = null;
-				if(job.has("api")) {
-					if(job.getString("api").equals("0.12")) {
-						handler = new v012Callback();
-					}
-					
-					if(handler != null)
-						apiCallback.processNewStatus(handler.processJSON(job));
-				}
-			}
-			catch(JSONException e) {
-				throw new IOException(e);
+
+ 			Gson g = new Gson();
+			SpaceStatus ss = g.fromJson(content, SpaceStatus.class);
+			if(!ss.space.equals("")) {
+				apiCallback.processNewStatus(ss);
+				return true;
 			}
 		}
-		return null;
-	}
-	
-	private class v012Callback implements HSSAPIHandler {
-		@Override
-		public SpaceStatus processJSON(JSONObject job) throws JSONException {
-			SpaceStatus ss = new SpaceStatus();
-			JSONObject tmp = null;
-			
-			ss.api = job.getString("api");
-			ss.space = job.getString("space");
-			ss.logo = job.getString("logo");
-			ss.icon = new String[] {
-				job.getJSONObject("icon").getString("open"),
-				job.getJSONObject("icon").getString("closed")
-			};
-			ss.url = job.getString("url");
-			ss.address = job.optString("address");
-			if(job.has("contact")) {
-				ss.contact = new SpaceContact();
-				tmp = job.getJSONObject("contact");
-				ss.contact.phone = tmp.optString("phone");
-				ss.contact.sip = tmp.optString("sip");
-				if(tmp.has("keymaster")) {
-					String s = tmp.optString("keymaster");
-					if(s != "") {
-						
-					}
-				}
-				ss.contact.irc = tmp.optString("irc");
-				ss.contact.twitter = tmp.optString("twitter");
-				ss.contact.email = tmp.optString("email");
-				ss.contact.ml = tmp.optString("ml");
-				ss.contact.jabber = tmp.optString("jabber");
-			}
-			
-			return ss;
-		}
-		
-	}
-	
-	private interface HSSAPIHandler {
-		public SpaceStatus processJSON(JSONObject job) throws JSONException;
+		return false;
 	}
 	
 	public interface APICallback {
 		public void processNewStatus(SpaceStatus fresh);
 	}
-	public interface SpaceSensor {}
-	
-	public class SpaceStatus {
+
+	public static class SpaceStatus {
 		public String api;
 		public String space;
 		public String logo;
-		public String[] icon;
+		public SpaceIcons icon;
 		public String url;
 		public String address;
 		public SpaceContact contact;
@@ -125,10 +86,18 @@ public class HackerSpaceStatusAPI implements ResponseHandler<String> {
 		public String status;
 		public long lastchange;
 		public SpaceEvent[] events;
-		public SpaceSensor sensors;
-		public SpaceFeed feeds;
+		public Map<String, Map<String,String>>[] sensors;
+		public SpaceFeed[] feeds;
+		
+		public SpaceStatus() {}
 	}
-	public class SpaceContact {
+	public static class SpaceIcons {
+		public String open;
+		public String closed;
+		
+		public SpaceIcons() {}
+	}
+	public static class SpaceContact {
 		public String phone;
 		public String sip;
 		public String[] keymaster;
@@ -137,20 +106,22 @@ public class HackerSpaceStatusAPI implements ResponseHandler<String> {
 		public String email;
 		public String ml;
 		public String jabber;
+		
+		public SpaceContact() {}
 	}
-	public class SpaceEvent {
+	public static class SpaceEvent {
 		public String name;
-		public SpaceEventType type;
+		public String type;
 		public long t;
 		public String extra;
+		
+		public SpaceEvent() {}
 	}
-	public class SpaceFeed {
+	public static class SpaceFeed {
 		public String name;
 		public String type;
 		public String url;
+		
+		public SpaceFeed() {}
 	}
-	public enum SpaceEventType {
-		CHECK_IN, CHECK_OUT
-	}
-	
 }
--- src/de/ring0/hackspace/HackspaceWidgetConfig.java
+++ src/de/ring0/hackspace/HackspaceWidgetConfig.java
@@ -2,12 +2,29 @@ package de.ring0.hackspace;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
+import de.ring0.hackspace.HackerSpaceStatusAPI.APICallback;
+import de.ring0.hackspace.HackerSpaceStatusAPI.SpaceStatus;
 
-public class HackspaceWidgetConfig extends Activity {
+public class HackspaceWidgetConfig extends Activity implements APICallback {
+	HackerSpaceStatusAPI http;
+	SpaceStatus status = null;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        StrictMode.setThreadPolicy(ThreadPolicy.LAX);
+        
+        http = new HackerSpaceStatusAPI("http://status.kreativitaet-trifft-technik.de/status.json", this);
+        http.run();
     }
+
+	@Override
+	public void processNewStatus(SpaceStatus fresh) {
+		status = fresh;
+	}
 }
